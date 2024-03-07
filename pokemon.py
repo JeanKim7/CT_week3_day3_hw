82ca4504bdb3833380523a352ee60e127434ee48
JeanKim7 <jean.g.km@gmail.com>
2024-03-06 22:48:30 -0800

    Add opion to get info on berry

--- pokemon.py
+++ pokemon.py
@@ -3,9 +3,12 @@ from wrappers import PokemonAPI
 def main():
     print("Welcome to the Pokedex! Enter the name of a pokemon and we will give you some information about it.")
     pokemon = PokemonAPI()
+    berry = PokemonAPI()
     while True:
         input1 = input("What Pokemon would you like to learn about? ")
         print(pokemon.get_pokemon_info(input1))
+        berry_input = input("Enter a berry name to get information on it: ")
+        print(berry.get_berry_info(berry_input))
         continue_input = input("Enter anything to continue. Enter 'quit' to quit ")
         if continue_input == 'quit':
             break
--- wrappers.py
+++ wrappers.py
@@ -1,5 +1,16 @@
 import requests
 
+class Berry:
+
+    def __init__(self, name, flavor, size, smoothness):
+        self.name = name
+        self.flavor = flavor
+        self.size = size
+        self.smoothness = smoothness
+
+    def __str__(self):
+        return f"{self.name.title()} has a {self.flavor} flavor, size of {self.size} and smoothness of {self.smoothness}"
+
 class Pokemon:
 
     def __init__(self, name, height, weight, move1):
@@ -14,6 +25,29 @@ class Pokemon:
 class PokemonAPI:
     """A class that represents the data from a website"""    
     base_url = "https://pokeapi.co/api/v2/"
+    
+    def __get_berry(self, berry_name):
+        request_url = f"{self.base_url}berry/{berry_name}"
+        response = requests.get(request_url)
+        if response.ok:
+            data=response.json()
+            return data
+        else:
+            print(response.json())
+            return None
+        
+    def get_berry_info(self, berry_name1):
+            berry_info = self.__get_berry(berry_name1)
+            if berry_info:
+                berry_name=berry_name1
+                berry_flavors = (f"{berry_info['flavors'][0]['flavor']['name']} and {berry_info['flavors'][1]['flavor']['name']}")
+                berry_size = berry_info['size']
+                berry_smoothness = berry_info['smoothness']
+                berry_instance = Berry(berry_name, berry_flavors, berry_size, berry_smoothness)
+                return berry_instance
+            else:
+                return "No info on this berry could be found"
+
     
     def __get_pokemon(self, pokemon_species):    
         request_url = f"{self.base_url}pokemon/{pokemon_species}"
